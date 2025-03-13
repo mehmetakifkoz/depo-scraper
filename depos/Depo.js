dcf2f7ad6325bc0ac99e2ff3e5c4d0846c6f2ebe
Mehmet Akif KÖZ <mehmet_akif_koz@hotmail.com>
2025-03-13 15:35:38 +0300

    v1.1.2 - Remove automatic tab opening for barcode search
    - Removed the functionality that automatically opened a new tab if the depo was not found.
    - Now, barcode searches will only be performed in already open depo tabs.
    - This change allows users to control which depos they want to search in by closing unwanted tabs.

--- depos/Depo.js
+++ depos/Depo.js
@@ -22,14 +22,7 @@ export class Depo {
 
         // Query all tabs and check if the specific URL is already open
         chrome.tabs.query({ url: `${this.url}/*` }, (tabs) => {
-            // Handle case where no tab is found
-            if (tabs.length === 0) {
-                chrome.tabs.create({ url: this.url }, (tab) => {
-                    this.injectScript(tab.id, barcode);
-                });
-            } else {
-                this.injectScript(tabs[0].id, barcode);
-            }
+            this.injectScript(tabs[0].id, barcode);
         });
     }
 
