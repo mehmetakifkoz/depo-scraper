0b690096bae85ddd45253452f1870694de30be17
mehmetakifkoz <mehmet_akif_koz@hotmail.com>
2025-09-14 18:26:34 +0300

    Farmazonrx düzeltildi.

--- depos/Farmazonrx.js
+++ depos/Farmazonrx.js
@@ -5,7 +5,31 @@ export class Farmazonrx extends Depo {
         super();
         this.name = "Farmazonrx";
         this.url = "https://www.farmazonrx.com.tr";
-        this.searchAreaQuery = 'input.ui-input.has-icon-left.pr-6.is-header-search'; // Query for search area input
-        this.submitButtonQuery = 'button[type="submit"]'; // Query for submit button
+        this.searchMethod = "url";
+        this.barcodeIdHash = {};
+        this.urlSearchString = "";
+
+        // Load JSON asynchronously
+        this.loadBarcodeIdHash();
+    }
+
+    async loadBarcodeIdHash() {
+        try {
+            const response = await fetch(chrome.runtime.getURL("../cache/farmazonrx.json"));
+            this.barcodeIdHash = await response.json();
+        } catch (err) {
+            console.error("Failed to load barcodeIdHash:", err);
+            this.barcodeIdHash = {};
+        }
+    }
+
+    searchByUrl(tabId, barcode) {
+        let productId = this.barcodeIdHash[barcode];
+        this.urlSearchString = `https://www.farmazonrx.com.tr/product/${productId}`;
+        if (!this.urlSearchString) {
+            console.error(`URL search string is not defined for ${this.name}`);
+            return;
+        }
+        chrome.tabs.update(tabId, { url: this.urlSearchString });
     }
 }
